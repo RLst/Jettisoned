4822fe7deb38a02db376f54af896af65e2bd761b
RLst <letony@icloud.com>
2019-02-14 14:59:43 +1100

    Basic setup of the game with items and managers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+
+	////Prototype plan
+	//Make a bunch of useful items and class them as orbital objects
+	//
+
+}
---
+++ Assets/Scripts/OrbitalObject.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+public enum Atmosphere
+{
+
+    
+}
+
+public class OrbitalObject
+{
+    public float mass;
+    public float distanceFromEarth;
+
+    // public enum 
+    
+    //Mass
+    //Distance from
+}
